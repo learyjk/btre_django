97ee54ba2b0a08f6e9336810fe717f6cbabd60c9
Keegan Leary <leary.keegan@gmail.com>
2019-12-28 11:24:58 -0800

    Dynamic Content for Home, About, Listings, Listing

--- listings/models.py
+++ listings/models.py
@@ -13,7 +13,7 @@ class Listing(models.Model):
     description = models.TextField(blank=True)
     price = models.IntegerField()
     bedrooms = models.IntegerField()
-    bathroooms = models.DecimalField(max_digits=2, decimal_places=1)
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
     garage = models.IntegerField(default=0)
     sqft = models.IntegerField()
     lot_size = models.DecimalField(max_digits=5, decimal_places=1)
